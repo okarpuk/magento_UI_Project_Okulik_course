ee1e14f79258a1402ff6d37f0fe03c904685d70a
oleg <oleg.karpuk.1982@gmail.com>
2024-10-16 21:52:34 +0300

    locators added

--- pages/create_account.py
+++ pages/create_account.py
@@ -7,15 +7,23 @@ from pages.locators import create_account_locators as loc
 class CreateAccount(BasePage):
     page_url = '/customer/account/create/'
 
-    def fill_login_form(self, login, password):
+    def fill_create_account_form(self, login, password):
 
+        first_name_field = self.find_element(loc.first_name_field_loc)
+        last_name_field = self.find_element(loc.last_name_field_loc)
+        email_field = self.find_element(loc.email_field_loc)
+        password_field = self.find_element(loc.password_field_loc)
+        confirm_password_field = self.find_element(loc.confirm_password_field_loc)
+        create_account_button = self.find_element(loc.create_account_button_loc)
 
-        email_field = self.find(loc.email_field_loc)
-        password_field = self.find(loc.password_field_loc)
-        button = self.find(loc.button_loc)
-        email_field.send_keys(login)
+        first_name_field.send_keys(first_name)
+        last_name_field.send_keys(last_name)
+        email_field.send_keys(email)
         password_field.send_keys(password)
-        button.click()
+        confirm_password_field.send_keys(confirm_password)
+        create_account_button.click()
+
+
 
     def check_error_alert_text_is(self, text):
         WebDriverWait(self.driver, 5).until(project_ec.text_is_not_empty_in_element(loc.error_locator))
