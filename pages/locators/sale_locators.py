24539b3235614266191b1912371a840ddbfd4086
oleg <oleg.karpuk.1982@gmail.com>
2024-10-23 00:08:36 +0300

    sale partially added

--- pages/locators/sale_locators.py
+++ pages/locators/sale_locators.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-sale_page_name_loc = (By.XPATH, '//h1[@class="page-title"]')
+empty_cart_icon_loc = (By.XPATH, '//div[@data-block="minicart"]')
+empty_cart_message_loc = (By.XPATH, '//div[@id="minicart-content-wrapper"]')
 bags_link_loc = (By.XPATH, '//span[normalize-space()="Bags"]')
 mens_bargains_banner_loc = (By.XPATH, '//a[@class="block-promo sale-mens"]')
