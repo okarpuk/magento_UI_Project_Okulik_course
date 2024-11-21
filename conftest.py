f10b2b4fdeda136351e02ec0776144f24462cab1
oleg <oleg.karpuk.1982@gmail.com>
2024-11-21 08:40:14 +0300

    updates

--- conftest.py
+++ conftest.py
@@ -15,6 +15,8 @@ from pages.search_result import SearchResult
 def driver():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.maximize_window()
     return chrome_driver
